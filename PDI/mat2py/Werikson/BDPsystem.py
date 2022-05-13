8c779f6f5d3fb5845a53322839254a1a434e6cc6
WeriksonAlves <92264123+WeriksonAlves@users.noreply.github.com>
2022-05-13 14:26:24 -0300

    a

--- PDI/mat2py/Werikson/BDPsystem.py
+++ PDI/mat2py/Werikson/BDPsystem.py
@@ -1,6 +1,16 @@
-#                           PLATAFORMA BDP 2022
-#
-# Conjunto de abas que permite ao usuario ter acesso às  informações do sistema através da interface gráfica. 
+'''
+:::::::::::::::::::::::::::::::::::::::::::::::::::
+:Programadores: Mateus Souza e Werikson Alves   :
+:Data de início: 01/05/2022 - Data de término: ?? :
+:::::::::::::::::::::::::::::::::::::::::::::::::::
+
+PLATAFORMA BDP 2023
+
+Conjunto de janelas para as informações do sistema
+Através de um conjunto de botões será possível selecionar 
+as janelas que serão abertas e cada uma será correspondente às
+etapas de calibração, ajuste, comunicação e jogo.
+'''
 
 from tkinter import*
 import sys
@@ -16,40 +26,48 @@ class Menu_Principal(object):
     def __init__(self, **kw):
         #insira toda a inicialização aqui
                             
-        self.root = Tk()
-        self.root.title("PLATAFORMA BDP 2022")
-        self.root.geometry('300x500')
-        self.root.configure(bg='green')
-        self.create_status_bar()
-        self.create_menu_button()
-         
-    def create_status_bar(self):
+        self.root = Tk() #Cria a janela
+        self.root.title("PLATAFORMA BDP 2023") #Titulo da janela
+        self.root.geometry('300x500') # Tamanho da janela
+        self.root.configure(bg='green') # Cor de fundo da janela
+        self.create_status_bar() # Cria a notas de rodapé
+        self.create_menu_button() # Menu inicial do sistema
+    
+    # Cria a notas de rodapé
+    def create_status_bar(self): 
         self.status = Label(self.root,
                                text="Página Geral do Sistema",
                                bd=1, relief=SUNKEN, anchor=W)
         self.status.pack(side=BOTTOM, fill=X)
     
-    def clear_status_bar(self):
+    # Limpa a nota de rodapé
+    def clear_status_bar(self): 
         self.status.config(text="")
         self.status.update_idletasks() 
-         
-    def set_status_bar(self, texto):
+    
+    # Atualiza a nota de rodapé
+    def set_status_bar(self, texto): 
         self.status.config(text=texto)
         self.status.update_idletasks()       
 
-    def create_menu_button(self):
-        check_cam = Button(self.root, text= "Câmera", command = Menu_Camera)
+    def create_menu_button(self): # Menu inicial do sistema
+        # Abre a janela de configurações da camera
+        check_cam = Button(self.root, text= "Câmera", command = Menu_Camera) 
         check_cam.place(height=50, width=200, x=50, y=10)
 
+        # Abre a janela de calibração de cores
         check_cal_cor = Button(self.root, text= "Calibrar Cores", command = Menu_Calibra_Cor)
         check_cal_cor.place(height=50, width=200, x=50, y=70)
 
+        # Abre a janela de calibração de campo
         check_cal_cam = Button(self.root, text= "Calibrar Campo", command = Menu_Calibra_Campo)
         check_cal_cam.place(height=50, width=200, x=50, y=130)
 
+        # Abre a janela de configurações da partida
         check_hab_par = Button(self.root, text= "Habilitar Partida")
         check_hab_par.place(height=50, width=200, x=50, y=190)
 
+        #Encerra o software
         btn_sair = Button(self.root, text= "Encerrar o programa", bg='grey', activebackground='red', command=self.finaliza_software)
         btn_sair.place(height=50, width=200, x=50, y=250)
 
